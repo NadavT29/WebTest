bd024dec4d4caeb324590af097c4dae9874fb09e
unknown <nadavtoochman29@gmail.com>
2023-04-12 15:26:00 +0300

    change api key

--- src/actions/getImg.js
+++ src/actions/getImg.js
@@ -4,7 +4,7 @@ export const getimg = () => {
 
     return async (dispatch) => {
 
-        const response = await fetch('https://api.unsplash.com/search/photos?query=nature&page=1&per_page=5&client_id=aa7da31d016ad86222602dee2e4e588ec41b6308d5964450b74914c4ad7c00b5')
+        const response = await fetch('https://api.unsplash.com/search/photos?query=nature&page=1&per_page=5&client_id=cjwMEBJFzg_Qa1EpHsCYbxmm98ZnXEYfXiGTXuvnPP0')
 
         let i = await response.json();
         let backgrounds = i.results;
